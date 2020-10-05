cd7dd382c1bddc8965df37b6c8a7f9e6ea4886f5
JonathanDias04 <jonathangoncalves.dias2001@gmail.com>
2020-10-05 17:01:14 -0300

    Ajuste

--- js/funcoes.js
+++ js/funcoes.js
@@ -115,48 +115,6 @@ function like_post(data){
     })
 };
 
-function like_post(data){
-    var post_id = $(data).data('id');
-    $btn_clicado = $(data);
-    
-
-    if($btn_clicado.hasClass("hide")){
-        action = 'like';
-    } else if ($btn_clicado.hasClass("fa-thumbs-up")) {
-        action = 'unlike';
-    }
-
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    $.ajax ({
-        type: 'post',
-        url: "like_post",
-        data: {
-            'action': action,
-            'post_id': post_id 
-        },
-        success: function(data){
-            res = JSON.parse(data);
-
-            if(action == 'like') {
-                $btn_clicado.removeClass('fa-thumbs-o-up');
-                $btn_clicado.addClass('fa-thumbs-up');
-            } else if(action = 'unlike') {
-                $btn_clicado.removeClass('fa-thumbs-up');
-                $btn_clicado.addClass('fa-thumbs-o-up');
-            }
-
-            $btn_clicado.siblings('span.like_count').text(res.likes);
-        }
-    })
-};
-
-
-
 function update() {
     var input = document.getElementById('file'); //define o id do input
     var infoArea = document.getElementById( 'file-name' );//define o id do resutado do script
