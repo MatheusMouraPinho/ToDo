dbb594f6cdfc7991e6b0fd56bc61d6e50b138273
MatheusM <Matheusmpinho@outlook.com>
2020-11-22 21:32:02 -0200

    exemplo login

--- ToDo/resources/views/auth/login.blade.php
+++ ToDo/resources/views/auth/login.blade.php
@@ -5,61 +5,64 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card-login">
-               
+                <div class="row card-corpo">
+                    <div class="col-8">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
 
-                <div class="card-corpo">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right"></label>
+    
+                                <div class="col-md-6">
+                                    <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right"></label>
- 
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right"></label>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right"></label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Senha">
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Senha">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-lg btn-primaria btn-block">
+                                        {{ __('Login') }}
+                                    </button>
+                                    <br><br>
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-linkk" href="{{ route('password.request') }}">
+                                            {{ __('Esqueceu Sua Senha?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                        
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn btn-lg btn-primaria btn-block">
-                                    {{ __('Login') }}
-                                </button>
-                                <br><br>
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-linkk" href="{{ route('password.request') }}">
-                                        {{ __('Esqueceu Sua Senha?') }}
-                                    </a>
-                                @endif
+                                <a class="btn btn-linkk" href="{{ route('register') }}">
+                                    Não Possui Conta?
+                                </a>
                             </div>
                         </div>
-                    </form>
-                    
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <a class="btn btn-linkk" href="{{ route('register') }}">
-                                Não Possui Conta?
-                            </a>
-                        </div>
+                    </div>
+                    <div class="col-4">
+                        <img width="100px"src="{{asset('img/art1.png')}}">
                     </div>
                 </div>
             </div>
